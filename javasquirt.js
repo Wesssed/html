536db370084580671324992a5ae3ef40c119d5fa
Wesssed <116358813+Wesssed@users.noreply.github.com>
2023-03-04 22:01:55 +0200

    Add files via upload

---
+++ javasquirt.js
@@ -0,0 +1,44 @@
+
+let audio = document.getElementById("music");
+audio.volume = 0.0;
+
+ function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+let random = getRandomInt(6)
+switch (random){
+    case 1:
+        document.getElementById('music').src = `1.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"`
+      break;
+
+      case 2:
+        document.getElementById('music').src = `2.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/0HS6Lmeq0pvAhFoZ9Kazyu?utm_source=generator&amp;theme=0`
+      break;
+
+      case 3:
+        document.getElementById('music').src = `3.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/5ASxwnS9Y1EwxBsA9qmOV8?utm_source=generator&amp;theme=0`
+      break;
+
+      case 4:
+        document.getElementById('music').src = `4.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/1hGRe4d3LJCg1VszAU8Cy1?utm_source=generator&amp;theme=0`
+      break;
+
+      case 5:
+        document.getElementById('music').src = `5.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/16LldYYJefKzxIM6mc7ryR?utm_source=generator&amp;theme=0`
+      break;
+      case 6:
+        document.getElementById('music').src = `6.mp3`
+        document.getElementById('spopafi').src = `https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"`
+      break;
+    }
+  
+   
+
+
+
+print( )
