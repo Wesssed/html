8d6e718992c87f45b752be30bab98d0081ec28c1
Wesssed <116358813+Wesssed@users.noreply.github.com>
2023-03-04 23:57:01 +0200

    Update javasquirt.js

--- javasquirt.js
+++ javasquirt.js
@@ -4,31 +4,31 @@ audio.volume = 0.0;
  function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-let random = getRandomInt(5)
+let random = getRandomInt(4)
 let widget;
 let myzyaka;
 switch (random){
-      case 1:
+      case 0:
           myzyaka = "1.mp3"
           widget = "https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"
         break;
 
-        case 2:
+        case 1:
             myzyaka = "2.mp3"
             widget = "https://open.spotify.com/embed/track/0HS6Lmeq0pvAhFoZ9Kazyu?utm_source=generator&amp;theme=0"
         break;
 
-        case 3:
+        case 2:
             myzyaka = "3.mp3"
             widget = "https://open.spotify.com/embed/track/5ASxwnS9Y1EwxBsA9qmOV8?utm_source=generator&amp;theme=0"
         break;
 
-        case 4:
+        case 3:
             myzyaka = "4.mp3"
             widget = "https://open.spotify.com/embed/track/1hGRe4d3LJCg1VszAU8Cy1?utm_source=generator&amp;theme=0"
         break;
 
-        case 5:
+        case 4:
             myzyaka = "5.mp3"
             widget = "https://open.spotify.com/embed/track/16LldYYJefKzxIM6mc7ryR?utm_source=generator&amp;theme=0"
         break;
