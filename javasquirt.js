b9d3b982314d849075345c1cad6cb297ca955dbb
Wesssed <116358813+Wesssed@users.noreply.github.com>
2023-03-04 22:42:05 +0200

    Update javasquirt.js

--- javasquirt.js
+++ javasquirt.js
@@ -9,32 +9,32 @@ let widget;
 let myzyaka;
 switch (random){
     case 1:
-        myzyaka = `1.mp3`
-        widget = `https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"`
+        myzyaka = "1.mp3"
+        widget = "https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"
       break;
 
         case 2:
-            myzyaka = `2.mp3`
-            widget = `https://open.spotify.com/embed/track/0HS6Lmeq0pvAhFoZ9Kazyu?utm_source=generator&amp;theme=0`
+            myzyaka = "2.mp3"
+            widget = "https://open.spotify.com/embed/track/0HS6Lmeq0pvAhFoZ9Kazyu?utm_source=generator&amp;theme=0"
         break;
 
         case 3:
-            myzyaka = `3.mp3`
-            widget = `https://open.spotify.com/embed/track/5ASxwnS9Y1EwxBsA9qmOV8?utm_source=generator&amp;theme=0`
+            myzyaka = "3.mp3"
+            widget = "https://open.spotify.com/embed/track/5ASxwnS9Y1EwxBsA9qmOV8?utm_source=generator&amp;theme=0"
         break;
 
         case 4:
-            myzyaka = `4.mp3`
-            widget = `https://open.spotify.com/embed/track/1hGRe4d3LJCg1VszAU8Cy1?utm_source=generator&amp;theme=0`
+            myzyaka = "4.mp3"
+            widget = "https://open.spotify.com/embed/track/1hGRe4d3LJCg1VszAU8Cy1?utm_source=generator&amp;theme=0"
         break;
 
         case 5:
-            myzyaka = `5.mp3`
-            widget = `https://open.spotify.com/embed/track/16LldYYJefKzxIM6mc7ryR?utm_source=generator&amp;theme=0`
+            myzyaka = "5.mp3"
+            widget = "https://open.spotify.com/embed/track/16LldYYJefKzxIM6mc7ryR?utm_source=generator&amp;theme=0"
         break;
         case 6:
-            myzyaka = `6.mp3`
-            widget = `https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"`
+            myzyaka = "6.mp3"
+            widget = "https://open.spotify.com/embed/track/3YmZ8V92nqVKx6u1lohFCC?utm_source=generator&amp;theme=0"
         break;
         }
         document.getElementById('music').src = myzyaka;
